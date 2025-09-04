b9cca0b690ae33b73538727499c138c83e027a3b
wojciechprzyluski <przyluskiwojciech@gmail.com>
2025-09-04 20:59:41 +0200

    feat: add Cart types and constants

---
+++ src/types/Cart.ts
@@ -0,0 +1,17 @@
+import type { ProductInfo } from "./ProductInfo";
+
+export type CartItem = ProductInfo & {
+  id: string;
+  quantity: number;
+  addedAt: string;
+  updatedAt: string;
+};
+
+export type CartState = {
+  version: 1;
+  updatedAt: string;
+  items: CartItem[];
+};
+
+export const CART_LS_KEY = "cart";
+export const CART_VERSION = 1 as const;
